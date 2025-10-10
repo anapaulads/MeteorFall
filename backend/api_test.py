52b6ff44c030fe097053567495875b1c113144ff
anapaulads <contato.paulla@outlook.com>
2025-10-10 14:52:56 -0300

    Update app.py

--- backend/api_test.py
+++
@@ -1,9 +0,0 @@
-# backend/api_test.py
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-
-@app.route('/api/test')
-def test_endpoint():
-    return jsonify({"message": "O backend Python esta vivo!"})
